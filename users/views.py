04cfee396470f477ced87a52ec0b501a0fcbd1ac
ALTSisyphus <altsisyphus@gmail.com>
2026-09-02 21:39:24 +0300

    Реализовал получение и редактирование профиля пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    """Получение и редактирование профиля пользователя."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
