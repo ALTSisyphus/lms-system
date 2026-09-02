8b790b4ed5685797716bb87a214abd83c9577dbe
ALTSisyphus <altsisyphus@gmail.com>
2026-09-02 21:30:09 +0300

    Создал кастомную модель пользователя

---
+++ users/models.py
@@ -0,0 +1,97 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email обязателен")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Суперпользователь должен иметь is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(
+                "Суперпользователь должен иметь is_superuser=True"
+            )
+
+        return self.create_user(
+            email=email,
+            password=password,
+            **extra_fields,
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+        verbose_name="email",
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name="имя",
+    )
+
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name="фамилия",
+    )
+
+    phone = models.CharField(
+        max_length=35,
+        blank=True,
+        verbose_name="телефон",
+    )
+
+    city = models.CharField(
+        max_length=150,
+        blank=True,
+        verbose_name="город",
+    )
+
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        blank=True,
+        null=True,
+        verbose_name="аватар",
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+        verbose_name="статус персонала",
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name="активен",
+    )
+
+    date_joined = models.DateTimeField(
+        default=timezone.now,
+        verbose_name="дата регистрации",
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
