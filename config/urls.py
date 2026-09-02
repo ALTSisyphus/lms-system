4d520ee1bc2a035659ea196fb37b43e56fbfbd4f
ALTSisyphus <altsisyphus@gmail.com>
2026-09-02 21:39:24 +0300

    Подключил маршруты профилей пользователей

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("materials.urls")),
+    path("api/", include("users.urls")),
 ]
 
 
