bf97110653005785922e54ead97f8c3751313a6d
ALTSisyphus <altsisyphus@gmail.com>
2026-09-03 13:53:33 +0300

    Добавил вывод и фильтрацию платежей

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import Payment, User
+from users.serializers import PaymentSerializer, UserSerializer
 
 
 class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
@@ -9,3 +10,33 @@ class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    """Получение списка платежей с фильтрацией и сортировкой."""
+
+    queryset = Payment.objects.select_related(
+        "user",
+        "paid_course",
+        "paid_lesson",
+    ).all()
+    serializer_class = PaymentSerializer
+
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    )
+
+    filterset_fields = (
+        "paid_course",
+        "paid_lesson",
+        "payment_method",
+    )
+
+    ordering_fields = (
+        "payment_date",
+    )
+
+    ordering = (
+        "-payment_date",
+    )
