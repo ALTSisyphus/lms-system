d31eaa4b228578630c1693d373bcabe29e07ca6c
ALTSisyphus <altsisyphus@gmail.com>
2026-09-19 13:08:27 +0300

    Добавлены права доступа для владельцев и модераторов

---
+++ materials/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsModerator(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.groups.filter(name="Модераторы").exists()
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return self.has_permission(request, view)
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and obj.owner_id == request.user.pk
