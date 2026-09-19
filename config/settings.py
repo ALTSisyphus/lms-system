1334b5b71de85bd397fd425be7878876d884f566
ALTSisyphus <altsisyphus@gmail.com>
2026-09-19 13:08:27 +0300

    Настроена JWT-аутентификация в DRF

--- config/settings.py
+++ config/settings.py
@@ -129,5 +129,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
