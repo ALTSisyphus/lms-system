22cf294b4bf2567fbda136b6286734909555f5a8
ALTSisyphus <altsisyphus@gmail.com>
2026-09-19 13:08:27 +0300

    Добавлены сериализаторы регистрации и профиля пользователя

--- users/serializers.py
+++ users/serializers.py
@@ -26,6 +26,10 @@ class PaymentSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=False, allow_blank=False,
+        trim_whitespace=False,
+    )
     payments = PaymentSerializer(
         many=True,
         read_only=True,
@@ -36,6 +40,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = (
             "id",
             "email",
+            "password",
             "first_name",
             "last_name",
             "phone",
@@ -43,3 +48,37 @@ class UserSerializer(serializers.ModelSerializer):
             "avatar",
             "payments",
         )
+
+    def validate_email(self, value):
+        value = User.objects.normalize_email(value)
+        users = User.objects.filter(email__iexact=value)
+        if self.instance is not None:
+            users = users.exclude(pk=self.instance.pk)
+        if users.exists():
+            raise serializers.ValidationError(
+                "Пользователь с таким email уже существует."
+            )
+        return value
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        if password is not None:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        request = self.context.get("request")
+        if request is None or request.user.pk != instance.pk:
+            data.pop("last_name", None)
+            data.pop("payments", None)
+        return data
+
+
+class UserRegistrationSerializer(UserSerializer):
+    password = serializers.CharField(
+        write_only=True, trim_whitespace=False, allow_blank=False,
+    )
