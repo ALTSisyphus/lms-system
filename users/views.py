0e4a9f266a664eab2d040a587528630d45feacfe
ALTSisyphus <altsisyphus@gmail.com>
2026-09-19 13:08:28 +0300

    Реализованы регистрация и защищенные операции с пользователями

--- users/views.py
+++ users/views.py
@@ -1,12 +1,30 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import Payment, User
-from users.serializers import PaymentSerializer, UserSerializer
+from users.permissions import IsSelf
+from users.serializers import (
+    PaymentSerializer, UserRegistrationSerializer, UserSerializer,
+)
 
 
-class UserRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
-    """Получение и редактирование профиля пользователя."""
+class UserRegistrationAPIView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny]
+    authentication_classes = []
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.prefetch_related("payments").all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserRetrieveUpdateAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """Просмотр профиля, изменение и удаление собственной учетной записи."""
+
+    permission_classes = [IsAuthenticated, IsSelf]
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -21,6 +39,8 @@ class PaymentListAPIView(generics.ListAPIView):
         "paid_lesson",
     ).all()
     serializer_class = PaymentSerializer
+
+    permission_classes = [IsAuthenticated]
 
     filter_backends = (
         DjangoFilterBackend,
