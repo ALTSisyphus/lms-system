a58f02493a7008a082cbc90735e87241cc506c84
ALTSisyphus <altsisyphus@gmail.com>
2026-09-19 13:08:27 +0300

    Добавлены маршруты регистрации и управления пользователями

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from users.views import PaymentListAPIView, UserRetrieveUpdateAPIView
+from users.views import (
+    PaymentListAPIView, UserListAPIView, UserRegistrationAPIView,
+    UserRetrieveUpdateAPIView,
+)
 
 
 urlpatterns = [
+    path("users/", UserListAPIView.as_view(), name="user-list"),
+    path("users/register/", UserRegistrationAPIView.as_view(),
+         name="user-register"),
     path(
         "users/<int:pk>/",
         UserRetrieveUpdateAPIView.as_view(),
